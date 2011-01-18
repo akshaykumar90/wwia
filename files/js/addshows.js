cfd996709a5f94dda5240c69b1f16d1b2b7e7de8
Akshay Kumar <akshaykumar90@gmail.com>
2011-01-18 19:08:19 +0530

    Added jQuery code to toggle <div> class
    - Grandparent <div> to the checkbox. Toggle between normalletter
      and checkedletter upon change of state of the checkbox.

--- files/js/addshows.js
+++ files/js/addshows.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
     $(':checkbox').change(function() {
+        // Toggle class of parent label
         var p = $(this).parent();
         if (p.hasClass('checked')) {
-            p.removeClass('checked').addClass('unchecked');
+            p.addClass('unchecked').removeClass('checked');
         } else {
-            p.removeClass('unchecked').addClass('checked');
+            p.addClass('checked').removeClass('unchecked');
+        }
+        
+        // Toggle class of grandparent div
+        var pp = p.parent();
+        if (pp.hasClass('checkedletter')) {
+            pp.addClass('normalletter').removeClass('checkedletter');
+        } else {
+            pp.addClass('checkedletter').removeClass('normalletter');
         }
     });
 });
